cde9c9fc4951bbfa7aca53d9a065fe661010cf1c
layton736 <ahmed.salame@stud.hs-mannheim.de>
2016-08-23 16:04:59 +0200

    Change Fullscreen modus entfernt

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,16 +7,14 @@ import com.mygdx.game.Main;
 
 public class DesktopLauncher {
 	
-	private final static int WINDOW_WIDTH = 800;
-
-	private final static int WINDOW_HEIGHT = 600;
-	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.width =  DesktopLauncher.WINDOW_WIDTH;
+		config.width =  Main.WINDOW_WIDTH;
 
-		config.height =  DesktopLauncher.WINDOW_HEIGHT;
+		config.height =  Main.WINDOW_HEIGHT;
+		
+		config.resizable = false;
 		
 		new LwjglApplication(new Main(), config);
 	}
